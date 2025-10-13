f3ba7607e1a8b406643ecb362a2b114ae1912b5c
Pradeep Parihar <prparihar90@gmail.com>
2025-10-13 21:56:28 +0530

    about section WIP

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -20,11 +20,6 @@ const About = () => {
               experience in multiple domains like Logistics, EdTech, Community
               App and Share Broker's App.
             </p>
-            <p>
-              I am a frontend developer from Mumbai, India having 4+ years of
-              experience in multiple domains like Logistics, EdTech, Community
-              App and Share Broker's App.
-            </p>
           </div>
           <div className="about-skills">
             <div className="about-skill"><p>HTML & CSS</p><hr style={{ width: "50%" }}/></div>
@@ -32,6 +27,17 @@ const About = () => {
             <div className="about-skill"><p>JavaScript</p><hr style={{ width: "60%" }}/></div>
             <div className="about-skill"><p>Next JS</p><hr style={{ width: "50%" }}/></div>
           </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>4+</h1>
+          <p>Years of Experience</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>12+</h1>
+          <p>Projects Completed</p>
         </div>
       </div>
     </div>
