cf94979459ffdb095819c615a65afd2b6aa59ac4
Pradeep Parihar <prparihar90@gmail.com>
2025-10-17 21:28:55 +0530

    Footer Section - WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import About from './Components/About/About'
 import Contact from './Components/Contact/Contact'
 import Experiences from './Components/Experiences/Experiences'
+import Footer from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
 import MyProjects from './Components/MyProjects/MyProjects'
 import Navbar from './Components/Navbar/Navbar'
@@ -14,6 +15,7 @@ const App = () => {
       <Experiences />
       <MyProjects />
       <Contact />
+      <Footer />
     </div>
   )
 }
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,38 @@
+import './footer.css';
+import google_logo from '../../assets/google-logo-svg.svg'
+import user_icon from '../../assets/user_icon.svg'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer-top">
+            <div className="footer-top-left">
+                <img src={google_logo} alt="" />
+                <p>I am a frontend developer from Mumbai, India having 4+ years of
+              experience in multiple domains like Logistics, EdTech, Community
+              App and Share Broker's App.</p>
+            </div>
+            <div className="footer-top-right">
+                <div className="footer-email-input">
+                    <img src={user_icon} alt="" />
+                    <input type="email" placeholder='Enter Your Email' />
+                </div>
+                <div className="footer-subscribe">Subscribe</div>
+            </div>
+        </div>
+        <hr />
+        <div className="footer-bottom">
+          <p className="footer-bottom-left">
+            © Pradeep Parihar. All Rights Reserved.
+          </p>
+          <div className="footer-bottom-right">
+            <p>Terms of Services</p>
+            <p>Privacy Policy</p>
+            <p>Connect With Me</p>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
