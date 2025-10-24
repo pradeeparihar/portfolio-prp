dbba2b9abb454564c417b0d44d5e1316f1c0a44a
Pradeep Parihar <prparihar90@gmail.com>
2025-10-24 18:13:37 +0530

    Navbar Responsive - WIP

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,17 +1,34 @@
 import './navbar.css'
 import logo from '../../assets/google-logo-svg.svg'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import underline from '../../assets/nav_underline.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import menu_open from '../../assets/menu_open.svg'
+import menu_close from '../../assets/menu_close.svg'
 
 const Navbar = () => {
 
-  const [menu, setMenu] = useState("About")
+  const [menu, setMenu] = useState("About");
+const menuRef = useRef<HTMLUListElement | null>(null);
+
+  const openMenu = () => {
+    if (menuRef.current) {
+      menuRef.current.style.right = "0";
+    }
+  }
+
+  const closeMenu = () => {
+    if (menuRef.current) {
+      menuRef.current.style.right = "-350px";
+    }
+  }
 
   return (
     <div className='navbar'>
         <img src={logo} alt='' />
-        <ul className="nav-menu">
+        <img src={menu_open} alt="" onClick={openMenu} className='nav-mob-open'/>
+        <ul ref={menuRef} className="nav-menu">
+          <img src={menu_close} alt="" onClick={closeMenu} className="nav-mob-close" />
             <li><AnchorLink className='anchor-link' href='#Home'><p onClick={() => setMenu("Home")}>Home</p></AnchorLink>{menu === "Home" ? <img src={underline} alt='' /> : <></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#About'><p onClick={() => setMenu("About")}>About</p></AnchorLink>{menu === "About" ? <img src={underline} alt='' /> : <></>}</li>
             <li><AnchorLink className='anchor-link' offset={50} href='#Experience'><p onClick={() => setMenu("Experience")}>Experience</p></AnchorLink>{menu === "Experience" ? <img src={underline} alt='' /> : <></>}</li>
